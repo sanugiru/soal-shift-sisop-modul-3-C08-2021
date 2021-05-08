d1138a0a836a3a29f5006dae117b5235ac2288c5
Shahnaaz Firdaus <72660860+sanugiru@users.noreply.github.com>
2021-05-08 21:33:18 +0700

    Add files via upload

---
+++ soal3/soal3.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<string.h>
+#include<pthread.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/wait.h>
+
+int main(int argc, char *argv[]){
+
+	if (argc > 2){
+
+		if (strcmp(argv[1],'-f') == 0){
+
+		}
+
+		else if (strcmp(argv[1],'-d') == 0){
+
+		}
+
+		else if (strcmp(argv[1],'*') == 0){
+
+		}
+
+		else {
+
+		}
+	}
+}
